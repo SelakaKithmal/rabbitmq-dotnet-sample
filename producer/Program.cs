7b5880ef082bccdb35e20695757f5c65c9467445
Selaka Nanayakkara <Selaka.Nanayakkara@cube.global>
2023-12-28 21:38:55 +0530

    Producer completed

--- producer/Program.cs
+++ producer/Program.cs
@@ -2,3 +2,16 @@
 using System.Text;
 using RabbitMQ.Client;
 
+var factory = new ConnectionFactory {HostName="localhost"};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue:"letterbox", durable:false,exclusive:false,autoDelete:false, arguments:null);
+
+var msg = "this is my first message";
+
+var encodedMsg = Encoding.UTF8.GetBytes(msg);
+
+channel.BasicPublish("", "letterbox", null, encodedMsg);
+
+Console.WriteLine($"Message published{msg}");
